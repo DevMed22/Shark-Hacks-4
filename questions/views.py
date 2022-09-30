f32501cf6c7158d7ee1adcae34a46a90218337bc
Anas Osama <anasosama914@gmail.com>
2022-09-30 23:46:24 +0200

    Questions and Answers

--- questions/views.py
+++ questions/views.py
@@ -1,12 +1,30 @@
 from django.views.generic import FormView, ListView, DetailView
 from .forms import addQuestionform
 from .models import QuesModel
-
-class Questions(ListView):
-    model = QuesModel
-    context_object_name = 'qs'
-    template_name = 'questions.html'
+from django.shortcuts import render
 
 
-class Answer(DetailView):
-    
+def home(request):
+    if request.method == 'POST':
+        print(request.POST)
+        questions=QuesModel.objects.all()
+        for q in questions:
+            print(request.POST.get(q.question))
+            # print(q.ans)
+            print()
+            ans = False
+            if str(q.ans) ==  str(request.POST.get(q.question)):
+                ans = True
+            else:
+                break
+
+        context = {
+            'ans':ans
+        }
+        return render(request,'result.html',context)
+    else:
+        questions=QuesModel.objects.all()
+        context = {
+            'questions':questions
+        }
+        return render(request,'questions.html',context)
